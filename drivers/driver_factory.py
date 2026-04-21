f6291c76525417248f96f845a7da4c0a9b24439c
KavijaSen <kavijaseneviratne@gmail.com>
2026-04-21 18:32:39 +0930

    Update Chrome options for Azure pipeline

--- drivers/driver_factory.py
+++ drivers/driver_factory.py
@@ -1,21 +1,16 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 class DriverFactory:
 
     @staticmethod
     def get_driver():
-        options = webdriver.ChromeOptions()
+        options = Options()
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
 
-        driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()),
-            options=options
-        )
-
+        driver = webdriver.Chrome(options=options)
         driver.implicitly_wait(10)
         return driver
