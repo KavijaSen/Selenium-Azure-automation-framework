fc31275a65870afcf124314514e0bb6bc08e7b28
KavijaSen <kavijaseneviratne@gmail.com>
2026-04-21 18:28:42 +0930

    Update Chrome options for Azure pipeline

--- drivers/driver_factory.py
+++ drivers/driver_factory.py
@@ -7,7 +7,10 @@ class DriverFactory:
     @staticmethod
     def get_driver():
         options = webdriver.ChromeOptions()
-        options.add_argument("--start-maximized")
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
 
         driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
