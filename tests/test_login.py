4bef42aae43564fad35f2f84d8d03cc7e8b3ca7f
KavijaSen <kavijaseneviratne@gmail.com>
2026-04-21 18:38:57 +0930

    Update test to use DriverFactory

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,24 +1,18 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from pages.login_page import LoginPage
-
+from driver_factory import DriverFactory
+from login_page import LoginPage
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
+    driver = DriverFactory.get_driver()
     yield driver
     driver.quit()
-
 
 def test_valid_login(driver):
     login_page = LoginPage(driver)
     login_page.open()
     login_page.login("tomsmith", "SuperSecretPassword!")
     assert "You logged into a secure area!" in login_page.get_flash_message()
-
 
 def test_invalid_login(driver):
     login_page = LoginPage(driver)
